3081bfe3312ad2bb5caf217d5013db244e763dcb
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2021-03-11 14:24:44 -0500

    Update items.py

--- course_scraper/items.py
+++ course_scraper/items.py
@@ -7,14 +7,4 @@ import scrapy
 
 
 class CourseScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    # course_id = scrapy.Field()
-    # name = scrapy.Field()
-    # city = scrapy.Field()
-    # state = scrapy.Field()
-    # name_url = scrapy.Field()
-    # city_url = scrapy.Field()
-    # tees = scrapy.Field()
-
     pass
